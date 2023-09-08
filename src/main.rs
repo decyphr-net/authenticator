ad208cc6bda4ce628f937fdadffbf5b9cac74d40
Aaron Sinnott <aaronsinnott93@gmail.com>
2023-09-08 07:58:19 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use actix_web::middleware::Logger;
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+
+#[get("/api/ping")]
+async fn ping() -> impl Responder {
+    const MESSAGE: &str = "Ping successfull";
+    HttpResponse::Ok().json(
+        serde_json::json!(
+            {
+                "status": "success",
+                "message": MESSAGE
+            }
+        )
+    )
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    }
+    env_logger::init();
+
+    println!("🚀 Server started successfully");
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .service(ping)
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
+}
